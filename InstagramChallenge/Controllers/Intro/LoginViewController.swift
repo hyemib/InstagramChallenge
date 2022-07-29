ab636aeff88e7deffa7d1aaccc5ba3f93702a16b
hhyemm <hyem0127@gmail.com>
2022-07-30 03:43:32 +0900

    Add : 로그인 화면 기능

--- InstagramChallenge/Controllers/Intro/LoginViewController.swift
+++ InstagramChallenge/Controllers/Intro/LoginViewController.swift
@@ -1,7 +1,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -10,14 +10,18 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     
     var isPasswordView = true
+    var enableLoginButton = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        idTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        passwordTextField.isSecureTextEntry = isPasswordView
+        
         setTextFieldDesign()
         setLoginButtonDesign()
-        
-        setTextField()
     }
     
     func setTextFieldDesign() {
@@ -25,17 +29,20 @@ class LoginViewController: UIViewController {
         passwordTextField.addLeftPaading(padding: 10)
     }
     
-    func setTextField() {
-        passwordTextField.isSecureTextEntry = isPasswordView
-        self.passwordTextField.addTarget(self, action: #selector(didChangeTextField(_:)), for: .editingChanged)
+    func checkTextFieldMaxLength(textField: UITextField!, maxLength: Int) {
+        if textField.text?.count ?? 0 > maxLength {
+            textField.deleteBackward()
+        }
+    }
+  
+    @IBAction func setIdTextField_(_ sender: Any) {
+        checkTextFieldMaxLength(textField: idTextField, maxLength: 20)
+        setEnableLoginButton()
     }
     
-    @objc func didChangeTextField(_ sender: Any?) {
-        if passwordTextField.text!.isEmpty {
-            loginButton.backgroundColor = .mainBlueBlurColor
-        } else {
-            loginButton.backgroundColor = .mainBlueColor
-        }
+    @IBAction func setPasswordTextField_(_ sender: Any) {
+        checkTextFieldMaxLength(textField: passwordTextField, maxLength: 20)
+        setEnableLoginButton()
     }
     
     @IBAction func changePasswordView(_ sender: Any) {
@@ -49,16 +56,67 @@ class LoginViewController: UIViewController {
         isPasswordView = !isPasswordView
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == idTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            pressLoginButton(loginButton)
+        }
+        return true
+    }
     
     func setLoginButtonDesign() {
         loginButton.layer.cornerRadius = loginButton.frame.height / 5
         loginButton.backgroundColor = .mainBlueBlurColor
     }
     
-    @IBAction func moveJoinButton(_ sender: UIButton) {
+    func setEnableLoginButton() {
+        if idTextField.text!.isEmpty && passwordTextField.text!.isEmpty {
+            loginButton.backgroundColor = .mainBlueBlurColor
+            enableLoginButton = false
+        } else {
+            loginButton.backgroundColor = .mainBlueColor
+            enableLoginButton = true
+        }
+    }
+    
+    func isValidLogin(textField: UITextField!, minLength: Int, maxLength: Int) -> Bool {
+        guard textField.text != nil else { return false }
+        var regEx = ""
+        if textField == idTextField {
+            regEx = "[A-Za-z0-9]{\(minLength),\(maxLength)}"
+        } else if textField == passwordTextField {
+            regEx = "[A-Za-z0-9!_@$%^&+=]{\(minLength),\(maxLength)}"
+        }
+        let pred = NSPredicate(format:"SELF MATCHES %@", regEx)
+        return pred.evaluate(with: textField.text)
+    }
+    
+    @IBAction func pressLoginButton(_ sender: UIButton) {
+        if !enableLoginButton { return }
+        if isValidLogin(textField: idTextField, minLength: 3, maxLength: 20) || isValidLogin(textField: passwordTextField, minLength: 6, maxLength: 20) {
+            guard let vc = self.storyboard?.instantiateViewController(identifier: "HomeViewController") as? HomeViewController else { return }
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: false, completion: nil)
+        } else {
+            let alret = UIAlertController(title: "계정을 찾을 수 없음", message: "\(idTextField.text!)에 연결된 계정을 찾을 수 없습니다. 다른 전화번호나 이메일 주소를 사용해보세요. Instagram 계정이 없으면 가입할 수 있습니다.", preferredStyle: .alert)
+            let join = UIAlertAction(title: "가입하기", style: .default) {_ in
+                self.moveJoinView()
+            }
+            let retry = UIAlertAction(title: "다시 시도", style: .default)
+            alret.addAction(join)
+            alret.addAction(retry)
+            present(alret, animated: true, completion: nil)
+        }
+    }
+    
+    func moveJoinView() {
         guard let vc = self.storyboard?.instantiateViewController(identifier: "JoinViewController") as? JoinViewController else { return }
         vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: false, completion: nil)
     }
+    
+    @IBAction func pressJoinButton(_ sender: UIButton) {
+        moveJoinView()
+    }
 }
-
