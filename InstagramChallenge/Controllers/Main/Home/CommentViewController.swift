e2fded9e1dc2ede6da721060cfe1a00d8ebc9438
hhyemm <hyem0127@gmail.com>
2022-07-29 16:31:10 +0900

    Add : 댓글 작성 화면

---
+++ InstagramChallenge/Controllers/Main/Home/CommentViewController.swift
@@ -0,0 +1,39 @@
+
+import UIKit
+
+class CommentViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+}
+
+extension CommentViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        }
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+             return cell
+        }
+       let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
