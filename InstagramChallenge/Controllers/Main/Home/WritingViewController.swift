d1eb4f31660abcc237934c44bdca1b96ec39451b
hhyemm <hyem0127@gmail.com>
2022-08-04 18:38:32 +0900

    Add : 게시글작성 화면

---
+++ InstagramChallenge/Controllers/Main/Home/WritingViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class WritingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func goPhotoSelectView(_ sender: UIButton) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    
+
+}
