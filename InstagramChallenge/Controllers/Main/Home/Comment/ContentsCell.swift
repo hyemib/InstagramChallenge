3c10b9a1aa77fd043c8b29c2fbfa3704be9443cf
hhyemm <hyem0127@gmail.com>
2022-08-10 22:45:03 +0900

    Update : 댓글

---
+++ InstagramChallenge/Controllers/Main/Home/Comment/ContentsCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContentsCell.swift
+//  InstagramChallenge
+//
+//  Created by hyemi on 2022/08/10.
+//
+
+import UIKit
+
+class ContentsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstagramChallenge/Models/Feed/CommentContents.swift
@@ -0,0 +1,8 @@
+//
+//  CommentContents.swift
+//  InstagramChallenge
+//
+//  Created by hyemi on 2022/08/10.
+//
+
+import Foundation
